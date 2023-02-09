516b5479fbb4dac966a503be3e212d228d61f46d
O-Z-Z-Y <lhsddp@naver.com>
2023-02-09 11:44:40 +0900

    complete sign-up-modal-layout

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -7,6 +7,11 @@ import PersonIcon from "../../public/static/svg/auth/person.svg"
 import OpenedEyeIcon from "../../public/static/svg/auth/opened_eye.svg"
 import ClosedEyeIcon from "../../public/static/svg/auth/closed_eye.svg"
 import Input from "../common/Input";
+import palette from "../../styles/palette";
+
+import { monthList, dayList, yearList } from "../../lib/staticData";
+import Selector from "../common/Selector";
+import Button from "../common/Button";
 
 const Container = styled.form`
   width: 568px;
@@ -24,6 +29,37 @@ const Container = styled.form`
     position: relative;
     margin-bottom: 16px;
   }
+  .sign-up-birthday-label {
+    font-size: 16px;
+    font-weight: 600;
+    margin-top: 16px;
+    margin-bottom: 8px;
+  }
+
+  .sign-up-modal-birthday-info {
+    margin-bottom: 16px;
+    color: ${palette.charcoal};
+  }
+  .sign-up-modal-birthday-selectors {
+    display: flex;
+    margin-bottom: 24px;
+    .sign-up-modal-birthday-month-selector {
+      margin-right: 16px;
+      flex-grow: 1;
+    }
+    .sign-up-modal-birthday-day-selector {
+      margin-right: 16px;
+      width: 25%;
+    }
+    .sign-up-modal-birthday-year-selector {
+      width: 33.3333%;
+    }
+  }
+  .sign-up-modal-submit-button-wrapper {
+    margin-bottom: 16px;
+    padding-bottom: 16px;
+    border-bottom: 1px solid ${palette.gray_eb};
+  }
 `;
 
 const SignUpModal: React.FC = () => {
@@ -33,6 +69,10 @@ const SignUpModal: React.FC = () => {
   const [password, setPassword] = useState("");
 
   const [hidePassword, setHidePassword] = useState(true);
+
+  const [birthYear, setBirthYear] = useState<string | undefined>();
+  const [birthDay, setBirthDay] = useState<string | undefined>();
+  const [birthMonth, setBirthMonth] = useState<string | undefined>();
 
   //* 비밀번호 숨김 토글하기
   const toggleHidePassword = () => {
@@ -57,6 +97,20 @@ const SignUpModal: React.FC = () => {
   const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
+
+  //* 생년월일 "월" 변경 시
+  const onChangeBirthMonth = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthMonth(event.target.value);
+  };
+  //* 생년월일 "일" 변경 시
+  const onChangeBirthDay = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthDay(event.target.value);
+  }
+  //* 생년월일 "년" 변경 시
+  const onChangeBirthYear = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthYear(event.target.value);
+  }
+  
   return (
   <Container>
     <CloseXIcon className="modal-close-x-icon" />
@@ -82,6 +136,42 @@ const SignUpModal: React.FC = () => {
       }
       value={password}
       onChange={onChangePassword} />
+    </div>
+    <p className="sign-up-birthday-label">생일</p>
+    <p className="sign-up-modal-birthday-info">
+      만 18세 이상의 성인만 회원으로 가입할 수 있습니다. 생일은 다른 에어비엔비 이용자에게 공개되지 않습니다.
+    </p>
+    <div className="sign-up-modal-birthday-selectors">
+      <div className="sign-up-modal-birthday-month-selector">
+        <Selector 
+          options={monthList}
+          disabledOptions={["월"]}
+          defaultValue="월"
+          value={birthMonth}
+          onChange={onChangeBirthMonth}
+        />
+      </div>
+      <div className="sign-up-modal-birthday-day-selector">
+        <Selector 
+          options={dayList}
+          disabledOptions={["일"]}
+          defaultValue="일"
+          value={birthDay}
+          onChange={onChangeBirthDay}
+        />
+      </div>
+      <div className="sign-up-modal-birthday-year-selector">
+        <Selector 
+          options={yearList}
+          disabledOptions={["년"]}
+          defaultValue="년"
+          value={birthYear}
+          onChange={onChangeBirthYear}
+        />
+      </div>
+    </div>
+    <div className="sign-up-modal-submit-button-wrapper">
+      <Button type="submit">가입하기</Button>
     </div>
   </Container>
   );
